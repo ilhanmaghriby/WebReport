d938fc0f7d4ed1f4723b1a0765a1753f1ead2024
ilhanmaghriby <ilhan.maghriby23@gmail.com>
2025-08-04 15:06:01 +0700

    remove cors

--- backend/server.js
+++ backend/server.js
@@ -15,7 +15,6 @@ const Report = require("./models/Report"); // ⬅️ Tambahkan ini
 const app = express();
 
 // Middleware
-app.use(cors());
 app.use(express.json());
 app.use("/uploads", express.static(path.join(__dirname, "uploads")));
 
