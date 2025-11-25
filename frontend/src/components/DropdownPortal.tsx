bbfa232beb74e4587f407fc874338b11d8455dd0
ilhanmaghriby <ilhan.maghriby23@gmail.com>
2025-11-25 12:00:17 +0700

    update dropdown button

--- frontend/src/components/DropdownPortal.tsx
+++
@@ -1,7 +0,0 @@
-import { createPortal } from "react-dom";
-
-export default  function DropdownPortal = ({ children }: { children: React.ReactNode }) => {
-  const el = document.getElementById("dropdown-root");
-  if (!el) return null;
-  return createPortal(children, el);
-};
